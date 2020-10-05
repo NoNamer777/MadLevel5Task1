c87f2ab5fbb94791f47c7b0cd5381708e5200069
Oscar Wellner <o.wellner@telfort.nl>
2020-10-05 18:06:37 +0200

    Step 5: AddNoteFragment

--- app/src/main/java/com/nonamer777/madlevel5task1/model/NoteViewModel.kt
+++ app/src/main/java/com/nonamer777/madlevel5task1/model/NoteViewModel.kt
@@ -2,12 +2,57 @@ package com.nonamer777.madlevel5task1.model
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.nonamer777.madlevel5task1.repository.NoteRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.*
 
 class NoteViewModel(application: Application): AndroidViewModel(application) {
 
+    /** The Note Repository from which data is send to and received from. */
     private val noteRepo = NoteRepository(application.applicationContext)
 
+    /** An asynchronous scope in which mostly requests to the Note Repository are handled. */
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+
+    /** The Note that is to be edited. */
     val note = noteRepo.getNote()
 
+    /** An Error that could occur while updating a Note. */
+    val error = MutableLiveData<String>()
+
+    /** If updating a note is successful. */
+    val success = MutableLiveData<Boolean>()
+
+    /** Handles updating a Note. */
+    fun updateNote(title: String, body: String) {
+        // Create a new Note object with the received data.
+        val newNote = Note(
+            id = note.value?.id,
+            title = title,
+            lastUpdated = Date(),
+            body = body
+        )
+
+        // Handle updating the Note.
+        if (isNoteValid(newNote)) {
+            mainScope.launch {
+                withContext(Dispatchers.IO) { noteRepo.updateNote(newNote) }
+
+                success.value = true
+            }
+        }
+    }
+
+    /** Checks if the Note still has a title. */
+    private fun isNoteValid(note: Note): Boolean = when {
+        note.title.isBlank() -> {
+            error.value = "Title must be provided."
+            false
+        }
+        else -> true
+    }
 }
--- app/src/main/java/com/nonamer777/madlevel5task1/ui/AddNoteFragment.kt
+++ app/src/main/java/com/nonamer777/madlevel5task1/ui/AddNoteFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.nonamer777.madlevel5task1.R
-import kotlinx.android.synthetic.main.activity_main.*
+import com.nonamer777.madlevel5task1.model.NoteViewModel
+import kotlinx.android.synthetic.main.fragment_add_note.*
 
 /**
  * A simple [Fragment] subclass where a User can manipulate a Note.
  */
 class AddNoteFragment : Fragment() {
+
+    /** The view model for Notes. */
+    private val viewModel: NoteViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -22,9 +28,35 @@ class AddNoteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Set navigation action on the fab.
-        fab.setOnClickListener {
-            findNavController().navigate(R.id.action_notepadFragment_to_addNoteFragment)
-        }
+        // Handles click actions on the save button.
+        btnSaveNote.setOnClickListener { saveNote() }
+
+        observeNote()
     }
+
+    /** Sets up Notepad observers. */
+    private fun observeNote() {
+        // Fills the input fields with the data from the Note.
+        viewModel.note.observe(viewLifecycleOwner, { note -> note?.let {
+            inputNoteTitle.editText?.setText(it.title)
+            inputNoteBody.editText?.setText(it.body)
+        }})
+
+        // Shows an Error when the Note Title has been emptied.
+        viewModel.error.observe(viewLifecycleOwner, { message ->
+            Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+        })
+
+        // Navigates back on a successful update.
+        viewModel.success.observe(viewLifecycleOwner, {
+                success -> findNavController().popBackStack()
+        })
+    }
+
+    /** Requests the Notepad to be updated. */
+    private fun saveNote() = viewModel.updateNote(
+        inputNoteTitle.editText?.text.toString(),
+        inputNoteBody.editText?.text.toString()
+    )
+
 }
