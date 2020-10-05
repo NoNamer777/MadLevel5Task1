43c0f461e4c8d854113d3eae754e291c1aaf7cdc
Oscar Wellner <o.wellner@telfort.nl>
2020-10-05 15:15:32 +0200

    Step 1: Prerequisites

--- app/src/main/java/com/nonamer777/madlevel5task1/ui/AddNoteFragment.kt
+++ app/src/main/java/com/nonamer777/madlevel5task1/ui/AddNoteFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.nonamer777.madlevel5task1.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * A simple [Fragment] subclass where a User can manipulate a Note.
@@ -16,4 +18,13 @@ class AddNoteFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_add_note, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set navigation action on the fab.
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_notepadFragment_to_addNoteFragment)
+        }
+    }
 }
